0ebc47e0add98f926714c37677d5da97ea04dc57
[Gopal Kadam] <[gopalkadam212@gmail.com]>
2024-09-22 22:12:15 +0530

    Initial Setup 3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+      <h1 className="bg-blue-200 font-bold text-blue-600 mb-4">
         Welcome to My Tailwind CSS + React App
       </h1>
       <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition">
